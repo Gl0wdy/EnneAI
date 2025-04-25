42a5c9cd0e8bf9b05e55a64a6cecdb7b52b38d0c
Gl0wdy <183211804+Gl0wdy@users.noreply.github.com>
2025-04-25 21:19:15 +0300

    a

--- ai/utils.py
+++ ai/utils.py
@@ -5,7 +5,7 @@ import re
 
 PATH = '/bots/EnneAI/data/ennea'
 
-def get_enneadata(path: str = 'EnneAI/data/ennea'):
+def get_enneadata(path: str = PATH):
     res = []
     for i in os.listdir(path):
         with open(f'{path}/{i}', 'r', encoding='utf-8') as file:
